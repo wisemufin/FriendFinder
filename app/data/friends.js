73634b7b408ca6180d833bdefe0b76164a444d2d
Matthew Trabold <mct4240@gmail.com>
2019-04-08 21:30:55 -0500

    Working on API Routes
    Api routes in progress

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+  {
+    name: "Matthew",
+    photo: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5OS8zMjkvb3JpZ2luYWwvY2hpbXBzLWVhdC1tb25rZXktYnJhaW5zLTAx",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+  }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var friendData = require("../data/friends")
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+  });
+};
