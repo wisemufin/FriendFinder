02900a2fa45422561348c8b4a969c6910982839f
Matthew Trabold <mct4240@gmail.com>
2019-04-09 22:35:13 -0500

    Final Update
    500 Error :-1:

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
   {
     name: "Matthew",
     photo: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5OS8zMjkvb3JpZ2luYWwvY2hpbXBzLWVhdC1tb25rZXktYnJhaW5zLTAx",
-    scores: [
+    score: [
       1,
       2,
       3,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,13 +6,29 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    var lowestScore = 1000;
+    var matchedFriend = {
+      name: "",
+      photo: "",
+      lowestScore: 1000
+    }
+
+    var lowestScore = 100;
+    // var totalDifference = 0;
+    console.log(friendsData[0]);
+    console.log(friendsData.score[0]);
     for (var i = 0; i < friendsData.length; i++) {
-      var currentScore = 0;
-      for(var j = 0; j < friendsData[i].score.length; j++) {
-        currentScore += friendsData[i].score[j] - req.body.score[j]
+      var totalDifference = 0;
+      for(var j = 0; j < friendsData[i].score[j]; j++) {
+        totalDifference += Math.abs(parseInt(friendsData[i].score[j]) - parseInt(req.body.score[j]));
+
+        if(totalDifference < match.matchScore) {
+          match.name = friendsData[i].name;
+          match.photo = friendsData[i].photo;
+          match.matchScore = totalDifference;
+        }
       }
     }
     friendsData.push(req.body);
+    res.json(matchedFriend);
   });
 };
