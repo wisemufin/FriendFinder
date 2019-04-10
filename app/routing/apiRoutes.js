ca90dbc5536678a87aeac2cde8e90c1ab33f0a57
Matthew Trabold <mct4240@gmail.com>
2019-04-09 20:26:03 -0500

    API Routes logic updated
    Updated the logic on API Routes.js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,17 @@ var friendsData = require("../data/friends")
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    res.json(friendsData);d
+    res.json(friendsData);
   });
 
   app.post("/api/friends", function(req, res) {
+    var lowestScore = 1000;
+    for (var i = 0; i < friendsData.length; i++) {
+      var currentScore = 0;
+      for(var j = 0; j < friendsData[i].score.length; j++) {
+        currentScore += friendsData[i].score[j] - req.body.score[j]
+      }
+    }
     friendsData.push(req.body);
   });
 };
